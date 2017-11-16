cbfa8e275c6f06a4a10259c4e48640c83ab612d2
LoudSoftware <contact@loudsoftware.tk>
2017-11-16 10:36:27 -0500

    Made the back button picture act as a back button in the settingsview.

---
+++ app/src/main/java/com/example/mohammedabu/dutyhelper/dbHelpers/UserHelper.java
@@ -0,0 +1,60 @@
+package com.example.mohammedabu.dutyhelper.dbHelpers;
+
+import android.os.Debug;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by nicoz on 15-Nov-17.
+ */
+
+public class UserHelper {
+    private FirebaseUser currentUser;
+    private String UID;
+    private String name;
+    private String email;
+
+    public UserHelper(FirebaseAuth authInstance) {
+        currentUser = authInstance.getCurrentUser();
+        if (currentUser != null) {
+            name = currentUser.getDisplayName();
+            email = currentUser.getEmail();
+            UID = currentUser.getUid();
+        }
+    }
+
+
+    public void setUserEmail(String newEmail) {
+
+        if (currentUser != null && newEmail != null) {
+            currentUser.updateEmail(newEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "User email address updated.");
+                    }
+                }
+            });
+        }
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+}
