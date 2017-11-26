1ddb98644ed5ddeda40f08ef2e7a8a7630d5ac01
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2017-11-26 14:58:57 -0500

    Added the ability to move from the Calendar Page to the Create page and vice versa.

--- app/src/main/java/com/example/mohammedabu/dutyhelper/CalenderFragment.java
+++ app/src/main/java/com/example/mohammedabu/dutyhelper/CalenderFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +28,19 @@ public class CalenderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_calender, container, false);
+        calendarView = (CalendarView)view.findViewById(R.id.calendarView2);
+        button = (Button)view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //navigating from this fragment page to the create page once the create task is clicked.
+                Fragment fragment = new CreateFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().replace(getId(), fragment);
+                //fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
         return view;
     }
 
--- app/src/main/java/com/example/mohammedabu/dutyhelper/CreateFragment.java
+++ app/src/main/java/com/example/mohammedabu/dutyhelper/CreateFragment.java
@@ -3,11 +3,14 @@ package com.example.mohammedabu.dutyhelper;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 /**
@@ -16,17 +19,32 @@ import android.widget.Spinner;
 
 public class CreateFragment extends Fragment {
 
+    Button cancel;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_create, container, false);
-
         //Creating the Spinner with the user's name into the application.
         Spinner mySpinner = (Spinner)view.findViewById(R.id.create_UserSelection);
         //The Strings that will use the users names from the array string in the strings.xml file in resources.
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+
+        cancel = (Button)view.findViewById(R.id.buttonCancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //navigating from this fragment page to the calendar page once the cancel button is clicked
+                Fragment fragment = new CalenderFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().replace(getId(), fragment);
+                //fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
         return view;
     }
 
